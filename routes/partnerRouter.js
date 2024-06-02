607064a2c90f871e8c94e5a1e3d397b93a254c71
AlliedEJ <48832292+AlliedEJ@users.noreply.github.com>
2024-06-02 11:19:03 -0400

    JWT Auth

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -12,7 +12,7 @@ Partner.find()
 })
 .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
 Partner.create(req.body)
 .then(partner => {
     console.log('Partner Created ', partner);
@@ -22,11 +22,11 @@ Partner.create(req.body)
 })
 .catch(err => next(err));
 })
-.put((req, res) => {
+.put(authenticate.verifyUser, (req, res) => {
 res.statusCode = 403;
 res.end('PUT operation not supported on /partners');
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
 Partner.deleteMany()
 .then(response => {
     res.statusCode = 200;
@@ -46,11 +46,11 @@ Partner.findById(req.params.partnerId)
 })
 .catch(err => next(err));
 })
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
 res.statusCode = 403;
 res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
 Partner.findByIdAndUpdate(req.params.partnerId, {
     $set: req.body
 }, { new: true })
@@ -61,7 +61,7 @@ Partner.findByIdAndUpdate(req.params.partnerId, {
 })
 .catch(err => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
 Partner.findByIdAndDelete(req.params.partnerId)
 .then(response => {
     res.statusCode = 200;
